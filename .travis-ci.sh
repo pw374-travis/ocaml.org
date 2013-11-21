733ba985ebcf6d938b307179de92ed0e52edc99d
Philippe Wang <philippe.wang@gmail.com>
2013-11-21 23:38:20 +0000

    Update .travis-ci.sh

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -13,7 +13,7 @@ esac
 	 
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam libssl-dev
 export OPAMYES=1
 opam init 
 opam install ${OPAM_DEPENDS}
